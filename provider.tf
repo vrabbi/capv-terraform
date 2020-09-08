19eb073dbff8d136e1de389d330cbe2989dcf1ab
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2020-09-08 22:52:31 +0300

    Updated Version Dependencies for providers

--- provider.tf
+++ provider.tf
@@ -5,12 +5,23 @@ provider "kubernetes-alpha" {
 provider "kubectl" {
   config_path = "~/.kube/config"
 }
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
 terraform {
   required_version = ">= 0.13"
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.6.2"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 1.13.1"
+    }
+    kubernetes-alpha = {
+      source = "hashicorp/kubernetes-alpha"
+      version = "~> 0.2.0"
     }
   }
 }
